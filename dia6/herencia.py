28f0d03e3b0548a4ff12f15533a9c816cf68f20b
fyah1992 <carrascomiguel1992@gmail.com>
2023-12-26 21:58:27 +0100

    dia6

--- dia6/herencia.py
+++ dia6/herencia.py
@@ -1,1 +1,24 @@
-probrando
+#Herencia  de padres a hijos 
+
+#Crear clase genéricas para utilizar en otras clases, ejemplo clase animal aplicable a mamíferos, aves...
+
+class Animal:
+    def __init__(self, edad, color):
+        self.edad = edad
+        self.color = color
+
+    def nacer(self):
+        print("Este animal ha nacido")
+    
+
+class Pajaro(Animal):
+    pass
+
+
+piolin = Pajaro(2, 'rojo')
+
+piolin.nacer()
+print(piolin.color)
+
+#print(Pajaro.__bases__)  #Para saber de quien hereda
+#print(Animal.__subclasses__)
